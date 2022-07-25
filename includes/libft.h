43d10cb92ecdecce506623bbf50d366a4b08989e
Joachim Leroux <jleroux@c2r6s5.42lausanne.ch>
2022-07-25 16:06:04 +0200

    Norm of libft.h

--- includes/libft.h
+++ includes/libft.h
@@ -6,7 +6,7 @@
 /*   By: jleroux <marvin@42lausanne.ch>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/22 13:41:41 by jleroux           #+#    #+#             */
-/*   Updated: 2022/07/22 14:13:24 by jleroux          ###   ########.fr       */
+/*   Updated: 2022/07/25 16:05:26 by jleroux          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -93,14 +93,14 @@ typedef struct s_list
 	struct s_list	*next;
 }					t_list;
 
-t_list		*ft_lstnew(void *content);
-t_list		*ft_lstlast(t_list *lst);
-t_list		*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
-void		ft_lstadd_front(t_list **alst, t_list *new);
-int			ft_lstsize(t_list *lst);
-void		ft_lstadd_back(t_list **alst, t_list *new);
-void		ft_lstdelone(t_list *lst, void (*del)(void *));
-void		ft_lstclear(t_list **lst, void (*del)(void *));
-void		ft_lstiter(t_list *lst, void (*f)(void *));
+t_list	*ft_lstnew(void *content);
+t_list	*ft_lstlast(t_list *lst);
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
+void	ft_lstadd_front(t_list **alst, t_list *new);
+int		ft_lstsize(t_list *lst);
+void	ft_lstadd_back(t_list **alst, t_list *new);
+void	ft_lstdelone(t_list *lst, void (*del)(void *));
+void	ft_lstclear(t_list **lst, void (*del)(void *));
+void	ft_lstiter(t_list *lst, void (*f)(void *));
 
 #endif
